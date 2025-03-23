79cb059e3d82d8309d644abc93111c21dc0d4973
Akash-pradhan2468 <akash2468pradhan@gmail.com>
2025-03-23 07:48:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask,request,render_template
+import pickle
+flask_app=Flask(__name__)
+model=pickle.load(open("model.pkl",'rb'))
+
+
+@flask_app.route("/")
+def home():
+    return render_template("index.html")
+
+@flask_app.route("/predict",methods=["POST"])
+def predict():
+    float_features=[float(x) for x in request.form.values()]
+    features=np.array(float_features)
+    features=features.reshape(1,-1)
+    prediction=model.predict(features)
+    print(prediction)
+    ans=""
+    if prediction[0]==0:
+        ans="The person is not get placed"
+    else:
+        ans="The person is get Placed"
+    return render_template("index.html",prediction_text="{}".format(ans))
+
+if __name__=="__main__":
+    flask_app.run(debug=True)
